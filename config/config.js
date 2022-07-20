7e3cc292ecad1fd9bab3187df33d165190d38a3c
OleksaBohdan <bohdan.oleksa@gmail.com>
2022-07-20 14:59:35 +0200

    fixed config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: 3000 || process.env.PORT,
-  TOKEN: '5541857595:AAE1sYnKX0CTDLC4AvAojLId1W3LxEs4F8E' || process.env.TOKEN,
-  DB: 'mongodb://127.0.0.1:27017/tg_bot_dictionary' || process.env.DB,
+  PORT: process.env.PORT || 3000,
+  TOKEN: process.env.TOKEN || '5541857595:AAE1sYnKX0CTDLC4AvAojLId1W3LxEs4F8E',
+  DB: process.env.DB || 'mongodb://127.0.0.1:27017/tg_bot_dictionary',
 };
